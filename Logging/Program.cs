64c904ee54b5ce6f15e5ede8ed496d6a6b9d2dff
FacePunch1337 <rezol1337@gmail.com>
2022-03-28 22:26:46 +0200

    Добавьте файлы проекта.

---
+++ Logging/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NLog;
+
+
+namespace Logging
+{
+    internal class Program
+    {
+        private static NLog.Config.LoggingConfiguration config;
+
+        private static Logger logger;
+
+
+        static void Main(string[] args)
+        {
+            logger = NLog.LogManager.GetCurrentClassLogger();
+
+            Console.Write("Enter file name: ");
+            String path = Console.ReadLine();
+            Console.Write("\n");
+
+            try
+            {
+                using (FileStream fstream = File.OpenRead(path))
+                {
+                    byte[] data = new byte[fstream.Length];
+                    fstream.Read(data, 0, data.Length);
+                    // декодируем байты в строку
+                    string textFromFile = Encoding.Default.GetString(data);
+                    Console.WriteLine($"File data: {textFromFile}");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString());
+                Console.WriteLine("файл не найден.");
+            }
+
+        }
+
+
+
+
+        static void Main2(string[] args)
+        {
+
+            logger = NLog.LogManager.GetCurrentClassLogger();
+
+
+
+            Console.Write("Enter number: ");
+            String str = Console.ReadLine();
+            try
+            {
+                Console.WriteLine($"2 x {str} = {2 * Convert.ToInt32(str)}");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.ToString(), str);
+                Console.WriteLine("error");
+            }
+
+
+        }
+
+
+        static void Main_Runtime_Config(string[] args)
+        {
+            config = new NLog.Config.LoggingConfiguration();
+
+            var con = new NLog.Targets.ConsoleTarget("con");
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, con);
+
+            var file = new NLog.Targets.FileTarget("file")
+            {
+                FileName = "log.txt"
+            };
+
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, file);
+
+            NLog.LogManager.Configuration = config;
+
+            logger = NLog.LogManager.GetCurrentClassLogger();
+
+
+
+            logger.Trace("Trace msg");
+            NLog.LogManager.Shutdown();
+
+
+        }
+
+    }
+}
